9530feb196d1e578a5cf13f89873298586c53260
unknown <mohamed.belkahla19@gmail.co>
2021-10-21 15:02:33 +0100

    first commit

--- Blink_AnalogRead/Blink_AnalogRead.ino
+++ Blink_AnalogRead/Blink_AnalogRead.ino
@@ -8,33 +8,45 @@
 int Nombre_de_tour =0;
 int longueur_bretta=2.5;
 float diametre_pignon = 0.5;
-String linenumber="VW-L1";
-unsigned long elapsed=0,start=0,A_Duration=0,D_Duration=0,elapsedA=0,elapsedD=0;
+//***********************************************CONFIG*******************************************//
+//---just select segment and matched line number ------------------------------------------------//
+String linenumber="MH2/INR-L9";
+char server[] = "192.168.8.102";
+//--- specify ip address for the device---------------------------------------------------------//
+IPAddress ip(192, 168, 8, 99); 
+//***********************************************************************************************//
+String reponse="";
+String Start_Time ="00:00:00";
+unsigned long elapsed=0,A_Duration=0;
+unsigned long long int start=0;
 int i,N_Tour=0, sensor = 18;
+float mul_per_r=1;
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 // if you don't want to use DNS (and reduce your sketch size)
 // use the numeric IP instead of the name for the server:
 //IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
-char server[] = "192.168.8.103"; 
+
 //char server[] = "www.google.com";    // name address for Google (using DNS)
-TaskHandle_t TASK_Incrimente_Handler,TASK_Test_Handler ;
+TaskHandle_t TASK_Incrimente_Handler,TASK_Test_Handler ,blinkhandler,changestatehandler;
 SemaphoreHandle_t  SxBinarySemaphore,RQUBinarySemaphore;
 
 // Set the static IP address to use if the DHCP fails to assign
-IPAddress ip(10, 175, 40, 5);
+
 
 EthernetClient client;
 
 // define two tasks for Blink & AnalogRead
 void TaskBlink( void *pvParameters );
 void TASK_Incrimente( void *pvParameters );
+void Starttime( void *pvParameters );
+void changestate(void *pvParameters);
 LiquidCrystal_I2C lcd(0x27,20,4);
+
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   SxBinarySemaphore = xSemaphoreCreateBinary();
   RQUBinarySemaphore= xSemaphoreCreateBinary();
-
 
   lcd.init();
   lcd.clear();         
@@ -48,8 +60,10 @@ void setup() {
    Serial.begin(115200);
 
 pinMode(LED_BUILTIN, OUTPUT);
-
-pinMode(sensor, INPUT);           // set pin to input
+ Ethernet.begin(mac,ip);
+ delay(1000);
+pinMode(sensor, INPUT);  
+    //Serial.println(Ethernet.localIP());// set pin to input
 digitalWrite(sensor, HIGH);
 attachInterrupt(digitalPinToInterrupt(sensor), ISRoutine, FALLING);
    
@@ -65,7 +79,7 @@ attachInterrupt(digitalPinToInterrupt(sensor), ISRoutine, FALLING);
     ,  "TASK_Incrimente"   // A name N_Tourust for humans
     ,  255  // This stack size can be checked & adN_Tourusted by reading the Stack Highwater
     ,  NULL
-    ,  1  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    ,  3  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
     ,  &TASK_Incrimente_Handler );
 xTaskCreate(
    LCD_CONTROLLER
@@ -81,11 +95,23 @@ xTaskCreate(
     ,  "Blink"   // A name just for humans
     ,  255  // This stack size can be checked & adjusted by reading the Stack Highwater
     ,  NULL
+    ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    ,  &blinkhandler );
+//     xTaskCreate(
+//  Starttime
+//    ,  "Starttime"   // A name just for humans
+//    ,  255  // This stack size can be checked & adjusted by reading the Stack Highwater
+//    ,  NULL
+//    ,  1  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+//    ,  NULL );
+
+xTaskCreate(
+    changestate
+    ,  "Blink"   // A name just for humans
+    ,  500  // This stack size can be checked & adjusted by reading the Stack Highwater
+    ,  NULL
     ,  1  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,  NULL );
-
-
-
+    ,  &changestatehandler);
 
    
 
@@ -132,20 +158,26 @@ void TaskBlink(void *pvParameters)  // This is a task.
 //  // give the Ethernet shield a second to initialize:
 
 
-Ethernet.begin(mac);
-vTaskDelay(1000 / portTICK_PERIOD_MS);
- 
+//Ethernet.begin(mac);
+//
+//vTaskDelay(1000 / portTICK_PERIOD_MS);
+ int state=2;
   for (;;) // A Task shall never return or exit.
-  {
+ {
 
 xSemaphoreTake(RQUBinarySemaphore, portMAX_DELAY);
-  Serial.println("connecting...");
+ // Serial.println("connecting...");
+  
 if (client.connect(server, 3333)) {
+  
     Serial.println("connected");
-    // Make a HTTP request:
-     client.println("GET /"+linenumber+"/"+elapsedA+" HTTP/1.1");
+//Serial.println(elapsed);
+
+
+   
+     client.println("GET /arduino/"+linenumber+"/"+elapsed+"/"+Start_Time+"/"+state+"/"+(String)(N_Tour*mul_per_r)+" HTTP/1.1");
    // client.println("GET /search?q=arduino HTTP/1.1");
-    client.println("Host: 192.168.8.103");
+    client.println("Host: 192.168.8.102");
     client.println("Connection: close");
     client.println();
 
@@ -154,14 +186,22 @@ if (client.connect(server, 3333)) {
        while (client.available()) {
      
     char c = client.read();
-    Serial.print(c);
+    reponse = reponse+c;
+   //Serial.print(c);
+    
   }
+  if(Start_Time=="00:00:00"){
+  Start_Time=reponse.substring(46,54);
+  }
+  reponse="";
   }
   else {
     // kf you didn't get a connection to the server:
     Serial.println("connection failed");
-    Ethernet.begin(mac);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    Ethernet.begin(mac,ip);
+     vTaskDelay(3000 / portTICK_PERIOD_MS);
+    Serial.println(Ethernet.localIP());
+  
   }
     
     
@@ -174,7 +214,7 @@ if (client.connect(server, 3333)) {
   
   // if the server's disconnected, stop the client:
   if (!client.connected()) {
-    Serial.println();
+    //Serial.println();
     Serial.println("disconnecting.");
     client.stop();
 
@@ -191,7 +231,7 @@ if (client.connect(server, 3333)) {
 
 
 
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
 void TASK_Incrimente(void *pvParameters){
 
@@ -205,66 +245,140 @@ void TASK_Incrimente(void *pvParameters){
 N_Tour++;
 
    Serial.println("interrupted");
-   Serial.println(N_Tour);
+   //Serial.println(N_Tour);
 
 
     elapsed=millis()- start;
   //digitalWrite(LED_BUILTIN,!digitalRead(LED_BUILTIN));  
     Serial.println((String)elapsed +" numéro"+(String)N_Tour);
-     
-    if(elapsed<(1000*3)){
-  elapsedA=elapsed;
-  elapsedD=0;
-   A_Duration=A_Duration+elapsedA; 
-  xSemaphoreGive( RQUBinarySemaphore);
-  
-}else{elapsedD=elapsed;
-       elapsedA=0; 
-          D_Duration=D_Duration+elapsedD; 
-      
-  }
+     A_Duration=A_Duration+elapsed;
   
    start= millis();
-   
- 
-
-  
+  xSemaphoreGive( RQUBinarySemaphore);
    }    
  
   
   }
 
+
+
+
+
+
+  void changestate(void *pvParameters){
+
+unsigned long  limit=0;
+  for(;;){
+  resumehere:
+   // vTaskPrioritySet( changestatehandler,3 );
+   limit= (millis()-start);
+// Serial.println(limit);  
+
+if(limit>120000){
+
+  vTaskSuspend(blinkhandler);
+
+  int state=1;
+  int clientstatus=client.connect(server,3333);
+ // Serial.println("*******yyy***********");
+ // Serial.println(clientstatus);
+  if (clientstatus) {
+    Serial.println("connected");
+    client.println("GET /arduino/"+linenumber+"/"+0+"/"+Start_Time+"/"+state+"/"+(String)(N_Tour*mul_per_r)+" HTTP/1.1");
+    client.println("Host: 192.168.8.102");
+    client.println("Connection: close");
+    client.println();
+
+    while(client.available()==0){};
+    while (client.available()) { 
+    char c = client.read();
+   // reponse = reponse+c;
+    Serial.print(c);  
+  }
+  
+  }
+   else {
+    // kf you didn't get a connection to the server:
+    Serial.println("connection failed");
+    Ethernet.begin(mac,ip);
+     vTaskDelay(3000 / portTICK_PERIOD_MS);
+    Serial.println(Ethernet.localIP());
+    //goto resumehere;
+  
+  }
+    
+  
+
+  
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+
+    // do nothing forevermore:
+    
+ };
+ start=millis();
+  }
+ //  vTaskPrioritySet( changestatehandler,2 );
+  vTaskResume(blinkhandler);
+  
+  
+
+
+  
+  }}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
   void LCD_CONTROLLER(void *pvParameters){
 unsigned long times ;
-int millisec , tseconds,tminutes,seconds,theure;
+int millisec , tseconds,tminutes,seconds,theure,Tminutes;
 String DisplayTime;
-char A_TimeStamp[100], D_TimeStamp[100];
-
+char A_TimeStamp[100];
   for(;;){
 
 times = A_Duration;
-  millisec  = times % 100;
-  tseconds = times/1000;
+  millisec  = times % 1000;
+  tseconds = times/1000;//
   tminutes = tseconds / 60;
+  Tminutes=tminutes%60;
   seconds = tseconds % 60;
   theure=tminutes/60;
-  sprintf(A_TimeStamp, "%02d:%02d:%02d",theure, tminutes, seconds);
+  sprintf(A_TimeStamp, "%02d:%02d:%02d",theure, Tminutes, seconds);
  // DisplayTime = " "+ String(tminutes,DEC) + ":" + String(seconds,DEC) ;//+ ":" + String(millisec,DEC);
+  
+ 
 
 
-
- vTaskDelay(1000 / portTICK_PERIOD_MS);   
- lcd.clear();
+     lcd.backlight();
      lcd.setCursor(0,00);
     lcd.print("L_NUMBER: ");
     lcd.setCursor(10,0);
       lcd.print(linenumber);
-      
+     
      lcd.setCursor(0,01);
-    lcd.print("ELAPSED: ");
+    lcd.print("ST_Time: ");
     lcd.setCursor(10,01);
-       
-      lcd.print((String)elapsedA+"" );
+        lcd.print(Start_Time);
+      //lcd.print((String)elapsed+"" );
       
     lcd.setCursor(0,2);
    lcd.print("N_BRETTE: ")  ;
@@ -274,32 +388,10 @@ times = A_Duration;
    lcd.print("A_DURAT: ")  ;
     lcd.setCursor(10,3);
    lcd.print(A_TimeStamp);
-
-vTaskDelay(1000 / portTICK_PERIOD_MS);
-
-lcd.clear();
-  lcd.setCursor(0,00);
-    lcd.print("SERVER: ");
-    lcd.setCursor(7,0);
-      lcd.print(server);
-      
-     lcd.setCursor(0,01);
-    lcd.print("D_Durat(s):");
-    lcd.setCursor(10,01);
-       
-      lcd.print(D_Duration/1000 );
-      
-    lcd.setCursor(0,2);
-   lcd.print("")  ;
-    lcd.setCursor(10,2);
-   lcd.print("");
- lcd.setCursor(0,3);
-   lcd.print("")  ;
-    lcd.setCursor(10,3);
-   lcd.print("");
-    
+     
    }    
  
   
   }
- 
+
+  
