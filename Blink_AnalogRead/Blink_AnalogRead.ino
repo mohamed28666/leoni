51fdc7706a0324b3a2641033a6042c214d89bcde
unknown <mohamed.belkahla19@gmail.co>
2021-11-07 22:36:48 +0000

    first commit

--- Blink_AnalogRead/Blink_AnalogRead.ino
+++ Blink_AnalogRead/Blink_AnalogRead.ino
@@ -4,14 +4,16 @@
 #include <Ethernet2.h>
 #include <LiquidCrystal.h>
 #include <LiquidCrystal_I2C.h>
-
+const int RESET = 52;
 int Nombre_de_tour =0;
-int longueur_bretta=2.5;
+int longueur_bretta=2.5,hours=0,minutes=0;
 float diametre_pignon = 0.5;
+bool sending=0;
 //***********************************************CONFIG*******************************************//
 //---just select segment and matched line number ------------------------------------------------//
 String linenumber="MH2/INR-L9";
 char server[] = "192.168.1.41";
+unsigned long heureRAZmin=8.1e+7,heureRAZmax=heureRAZmin+10000 ;//en millisecondes
 //--- specify ip address for the device---------------------------------------------------------//
 IPAddress ip(192, 168, 1, 156); 
 IPAddress ipdefault(0,0,0,0);
@@ -19,12 +21,12 @@ IPAddress current(1,1,1,1);
 
 //***********************************************************************************************//
 String reponse="";
-String Start_Time ="00:00:00";
-unsigned long elapsed=0,A_Duration=0;
+String Start_Time ="00:00:00",timeSinceUpdate="00:00:00";
+unsigned long elapsed=0,A_Duration=0,millis_timeSincelastUpdate=0;
 unsigned long long int start=0;
 int i,N_Tour=0, sensor = 18;
 float mul_per_r=1;
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte mac[] = { 0xAE, 0xAA, 0xAE, 0xDF, 0xFE, 0x1D };
 // if you don't want to use DNS (and reduce your sketch size)
 // use the numeric IP instead of the name for the server:
 //IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
@@ -32,7 +34,16 @@ byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 //char server[] = "www.google.com";    // name address for Google (using DNS)
 TaskHandle_t TASK_Incrimente_Handler,TASK_Test_Handler ,blinkhandler,changestatehandler;
 SemaphoreHandle_t  SxBinarySemaphore,RQUBinarySemaphore;
-
+byte customChar[8] = {
+  0b00000,
+  0b01010,
+  0b11111,
+  0b11111,
+  0b01110,
+  0b00100,
+  0b00000,
+  0b00000
+};
 // Set the static IP address to use if the DHCP fails to assign
 
 
@@ -48,7 +59,10 @@ LiquidCrystal_I2C lcd(0x27,20,4);
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  
+    digitalWrite(RESET, HIGH); 
+ pinMode(RESET, OUTPUT);  
+    digitalWrite(RESET, HIGH);   
+
    Serial.begin(115200);
   lcd.init();
   lcd.clear();         
@@ -74,12 +88,14 @@ delay(2000);
   // initialize serial communication at 9600 bits per second:
 
 lcd.clear();     
+delay(1000);
 lcd.setCursor(0,1);
+
 lcd.print("Identifying ip...");
- 
-  initiate_cnx :
- Ethernet.begin(mac);
- delay(1000);
+
+initiate_cnx : 
+// Ethernet.maintain(); 
+ Ethernet.begin(mac,ip);
  Serial.println(Ethernet.localIP());
  current =Ethernet.localIP();
 
@@ -119,6 +135,14 @@ attachInterrupt(digitalPinToInterrupt(sensor), ISRoutine, FALLING);
     ,  3  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
     ,  &TASK_Incrimente_Handler );
   
+// xTaskCreate(
+//  remiseazero
+//    ,  "TASK_remiseazero"   // A name N_Tourust for humans
+//    ,  255  // This stack size can be checked & adN_Tourusted by reading the Stack Highwater
+//    ,  NULL
+//    ,  1  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+//    ,  NULL);
+//  
 
 
   
@@ -127,7 +151,7 @@ attachInterrupt(digitalPinToInterrupt(sensor), ISRoutine, FALLING);
     ,  "Blink"   // A name just for humans
     ,  255  // This stack size can be checked & adjusted by reading the Stack Highwater
     ,  NULL
-    ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    , 2 // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
     ,  &blinkhandler );
 //     xTaskCreate(
 //  Starttime
@@ -219,9 +243,12 @@ if (client.connect(server, 3333)) {
     client.println("Host: 192.168.8.102");
     client.println("Connection: close");
     client.println();
-
-    while(client.available()==0){};
+  while(client.available()==0){sending =1;};
+   
   
+//  vTaskDelay(300 / portTICK_PERIOD_MS);
+  
+   
        while (client.available()) {
      
     char c = client.read();
@@ -229,6 +256,13 @@ if (client.connect(server, 3333)) {
    //Serial.print(c);
     
   }
+  sending=0;
+   timeSinceUpdate=reponse.substring(46,54);
+    hours=timeSinceUpdate.substring(0,2).toInt();
+    minutes=timeSinceUpdate.substring(3,5).toInt();
+    millis_timeSincelastUpdate=((minutes*60000) +(hours*3.6*1000000));
+   Serial.println( millis_timeSincelastUpdate);
+    Serial.println(  timeSinceUpdate);
   if(Start_Time=="00:00:00"){
   Start_Time=reponse.substring(46,54);
   }
@@ -238,7 +272,8 @@ if (client.connect(server, 3333)) {
     // kf you didn't get a connection to the server:
     Serial.println("connection to server failed!!! (°_°)");
     //Ethernet.begin(mac,ip);
-     vTaskDelay(3000 / portTICK_PERIOD_MS);
+  //   vTaskDelay(3000 / portTICK_PERIOD_MS);
+     client.connect(server, 3333);
     Serial.println(Ethernet.localIP());
   
   }
@@ -312,7 +347,20 @@ unsigned long  limit=0;
    limit= (millis()-start);
   vTaskDelay(1000 / portTICK_PERIOD_MS);
  Serial.println("only "+(String)(120000-limit)+"ms and the assembly line will be set to PAUSED State");  
-
+ hours=timeSinceUpdate.substring(0,2).toInt();
+    minutes=timeSinceUpdate.substring(3,5).toInt();
+    millis_timeSincelastUpdate=((minutes*60000) +(hours*3.6*1000000));
+   Serial.println( "only "+ (String)(heureRAZmin -millis_timeSincelastUpdate-millis())+"s for starting shift 1/2");
+  
+  ////**************************** //
+   if( (millis_timeSincelastUpdate+millis())>(heureRAZmin) and  (millis_timeSincelastUpdate+millis())<(heureRAZmax) ){///// to verify ///////
+  
+    N_Tour=0;
+    Start_Time ="00:00:00";
+    
+    }
+//*********************************************///
+    Serial.println(  timeSinceUpdate);
 if(limit>120000){
 
   vTaskSuspend(blinkhandler);
@@ -340,6 +388,7 @@ if(limit>120000){
     // kf you didn't get a connection to the server:
     Serial.println("connection to server failed!!! (°_°)");
    // Ethernet.begin(mac,ip);
+
      vTaskDelay(3000 / portTICK_PERIOD_MS);
     Serial.println(Ethernet.localIP());
     //goto resumehere;
@@ -392,6 +441,8 @@ unsigned long times ;
 int millisec , tseconds,tminutes,seconds,theure,Tminutes;
 String DisplayTime;
 char A_TimeStamp[100];
+
+ lcd.createChar(0, customChar);
   for(;;){
 
 times = A_Duration;
@@ -404,7 +455,7 @@ times = A_Duration;
   sprintf(A_TimeStamp, "%02d:%02d:%02d",theure, Tminutes, seconds);
  // DisplayTime = " "+ String(tminutes,DEC) + ":" + String(seconds,DEC) ;//+ ":" + String(millisec,DEC);
   
- 
+
 
 
      lcd.backlight();
@@ -428,10 +479,23 @@ times = A_Duration;
     lcd.setCursor(4,3);
    //lcd.print(A_TimeStamp);
   lcd.print( current);
-     
+     if (sending==1){
+       lcd.setCursor(20,2);
+   lcd.write((byte)0);
+      }
    }    
  
   
   }
-
+void remiseazero(void){
+  String hours,minutes;
+  for(;;){
+   // Serial.println(timeSinceUpdate);
+    hours=timeSinceUpdate.substring(0,2);
+    minutes=timeSinceUpdate.substring(3,5);
+     Serial.println(hours+'-'+minutes);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+  
+  }
   
